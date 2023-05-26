72a719b81514f1cf596ba89c438af85788f49e24
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-26 16:26:35 +0530

    Single Submit Button

--- test_code/lib/main.dart
+++ test_code/lib/main.dart
@@ -1,88 +1,47 @@
-import 'dart:math';
-
-import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:slide_to_act/slide_to_act.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      title: 'Flutter Slide to act Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
     );
   }
 }
 
-class Post {
-  final String title;
-  final String body;
-
-  Post(this.title, this.body);
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final SearchBarController<Post> _searchBarController = SearchBarController();
-  bool isReplay = false;
-
-  Future<List<Post>> _getALlPosts(String text) async {
-    List<Post> posts = [];
-
-    var random = new Random();
-    for (int i = 0; i < 10; i++) {
-      posts
-          .add(Post("$text $i", "body random number : ${random.nextInt(100)}"));
-    }
-    return posts;
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SearchBar<Post>(
-          minimumChars: 1,
-          searchBarPadding: EdgeInsets.symmetric(horizontal: 10),
-          headerPadding: EdgeInsets.symmetric(horizontal: 10),
-          listPadding: EdgeInsets.symmetric(horizontal: 10),
-          onSearch: _getALlPosts,
-          searchBarController: _searchBarController,
-          placeHolder: Center(
-              child: Text(
-                "PlaceHolder",
-                style: TextStyle(fontSize: 30),
-              )),
-          cancellationWidget: Text("Cancel"),
-          emptyWidget: Text("empty"),
-          onCancelled: () {
-            print("Cancelled triggered");
-          },
-          mainAxisSpacing: 10,
-          onItemFound: (Post post, int index) {
-            return Container(
-              color: Colors.lightBlue,
-              child: ListTile(
-                title: Text(post.title),
-                isThreeLine: true,
-                subtitle: Text(post.body),
-                onTap: () {
-                  // Navigator.of(context)
-                  //     .push(MaterialPageRoute(builder: (context) => Detail()));
-                },
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SlideAction(
+            onSubmit: () {
+              // Add your logic here when the slide action is submitted
+              print('Slide action submitted!');
+            },
+            child: Text(
+              'Slide to Submit',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
-            );
-          },
+            ),
+            outerColor: Colors.blue,
+            innerColor: Colors.white,
+            sliderButtonIcon: Icon(Icons.arrow_forward),
+            borderRadius: 30,
+          ),
         ),
       ),
     );
   }
 }
-
