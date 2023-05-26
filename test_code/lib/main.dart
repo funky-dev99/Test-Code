c111bc9ce9f11d938f9fe6e9df803d2b73324f22
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-26 16:15:42 +0530

    Slide Button

--- test_code/lib/main.dart
+++ test_code/lib/main.dart
@@ -1,88 +1,240 @@
-import 'dart:math';
-
-import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:slide_to_act/slide_to_act.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      title: 'Flutter Slide to act Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
     );
   }
 }
 
-class Post {
-  final String title;
-  final String body;
-
-  Post(this.title, this.body);
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final SearchBarController<Post> _searchBarController = SearchBarController();
-  bool isReplay = false;
-
-  Future<List<Post>> _getALlPosts(String text) async {
-    List<Post> posts = [];
-
-    var random = new Random();
-    for (int i = 0; i < 10; i++) {
-      posts
-          .add(Post("$text $i", "body random number : ${random.nextInt(100)}"));
-    }
-    return posts;
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SearchBar<Post>(
-          minimumChars: 1,
-          searchBarPadding: EdgeInsets.symmetric(horizontal: 10),
-          headerPadding: EdgeInsets.symmetric(horizontal: 10),
-          listPadding: EdgeInsets.symmetric(horizontal: 10),
-          onSearch: _getALlPosts,
-          searchBarController: _searchBarController,
-          placeHolder: Center(
-              child: Text(
-                "PlaceHolder",
-                style: TextStyle(fontSize: 30),
-              )),
-          cancellationWidget: Text("Cancel"),
-          emptyWidget: Text("empty"),
-          onCancelled: () {
-            print("Cancelled triggered");
-          },
-          mainAxisSpacing: 10,
-          onItemFound: (Post post, int index) {
-            return Container(
-              color: Colors.lightBlue,
-              child: ListTile(
-                title: Text(post.title),
-                isThreeLine: true,
-                subtitle: Text(post.body),
-                onTap: () {
-                  // Navigator.of(context)
-                  //     .push(MaterialPageRoute(builder: (context) => Detail()));
-                },
-              ),
-            );
-          },
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Builder(
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reassemble(),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    innerColor: Colors.black,
+                    outerColor: Colors.white,
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Unlock',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    sliderButtonIcon: Icon(Icons.lock),
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    height: 100,
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    sliderButtonIconSize: 48,
+                    sliderButtonYOffset: -20,
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    elevation: 24,
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    borderRadius: 16,
+                    animationDuration: Duration(seconds: 1),
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    reversed: true,
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                    submittedIcon: Icon(
+                      Icons.done_all,
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final GlobalKey<SlideActionState> _key = GlobalKey();
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    key: _key,
+                    onSubmit: () {
+                      Future.delayed(
+                        Duration(seconds: 1),
+                            () => _key.currentState?.reset(),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SlideAction(
+                    sliderRotate: false,
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
